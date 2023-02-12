9e5cfa42d202e23df668731a7271ad65c25df89f
Angin Abroyan <anginabroyan@gmail.com>
2023-02-12 19:12:35 +0400

    Update Main.java

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -16,16 +16,5 @@ public class Main {
 //        arrays.oddsToTheEnd();
 //        arrays.noZeros();
 
-
-
-//        int[] n;
-//        System.out.println("Please enter the binary number: ");
-//        for(int i = 0; i < n.length; i++){
-//
-//        }
-//        arrays.binToDec(n);
-
-
-
     }
 }
